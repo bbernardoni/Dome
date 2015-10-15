fa9e902fdda311d31a3db54df21de35a87e260cd
bbernardoni <bennett.bernardoni@gmail.com>
2015-10-14 20:04:30 -0500

    Worked on day view

--- app/src/main/java/com/bbernardoni/dome/DayFrag.java
+++ app/src/main/java/com/bbernardoni/dome/DayFrag.java
@@ -1,6 +1,7 @@
 package com.bbernardoni.dome;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
@@ -8,9 +9,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class DayFrag extends Fragment {
+
+    RelativeLayout dayViewLayout;
+    CalEntries calEntries;
 
     public DayFrag() {
     }
@@ -22,9 +31,64 @@ public class DayFrag extends Fragment {
         assert ab != null;
         ab.setTitle(R.string.title_day);
 
-        return inflater.inflate(R.layout.fragment_day, container, false);
-    }
+        dayViewLayout = (RelativeLayout)container.findViewById(R.id.day_view_layout);
 
+        ViewTreeObserver vto = dayViewLayout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < 16) {
+                    //noinspection deprecation
+                    dayViewLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    dayViewLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                int width = dayViewLayout.getMeasuredWidth();
+                calEntries = ((MainActivity) getContext()).mCalEntries;
+                ArrayList<EventEntry> events = calEntries.getEventsOnDay(cal);
+                for (int k = 0; k < events.size(); k++) {
+
+                }
+            }
+        });
+
+        return inflater.inflate(R.layout.fragment_day, container, false);
+
+    }
+/*
+                    TextView tvPlusN;
+                    if(btnDayLayout.getChildCount()>1){
+                        tvPlusN = (TextView)btnDayLayout.getChildAt(1);
+                    }else{
+                        tvPlusN = new TextView(mContext);
+                        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                                RelativeLayout.LayoutParams.WRAP_CONTENT);
+                        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                        tvPlusN.setLayoutParams(params);
+                        tvPlusN.setTextSize(12.0f);
+                        tvPlusN.setTypeface(Typeface.DEFAULT_BOLD);
+                        tvPlusN.setTextColor(Color.WHITE);
+                        tvPlusN.setBackgroundResource(R.drawable.rounded_corner);
+                        btnDayLayout.addView(tvPlusN);
+                    }
+                    ((GradientDrawable)tvPlusN.getBackground()).setColor(btnDay.getCurrentTextColor());
+                    tvPlusN.setText("+" + (events.size() - numberOfTVs));
+
+<TextView
+    android:text="Event 1"
+    android:layout_marginTop="100dp"
+    android:layout_marginStart="0dp"
+    android:layout_marginLeft="0dp"
+    android:background="@drawable/rounded_corner"
+    android:layout_width="wrap_content"
+    android:layout_height="50dp" />
+
+<TextView
+    android:text="Event 2"
+    android:layout_width="wrap_content"
+    android:layout_height="50dp" />
+ */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
